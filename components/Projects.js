223086bf825d5065bbda85bcc346dd9245b668f7
leanhtoanqsb <leanhtoan1191@gmail.com>
2021-10-15 11:00:31 +0700

    1015

--- components/Projects.js
+++ components/Projects.js
@@ -13,10 +13,10 @@ export default function Projects() {
               <h2>Webtoon UI</h2>
             </a>
             <span><i>Personal project</i></span>
-            <p>Create a website whose interface looks like the Webtoon website</p>
-            <p>Compose the interface with React component, use React Router to navigate</p>
-            <p>Improve performance buy using Developer tools, React lazy</p>
-            <p>Used: <b><i>ReactJs, Hooks, HTML, CSS, Webpack </i></b><span>and depploy to </span><b><i>Netlify</i></b></p>
+            <p>Create a website whose interface looks like the Webtoon website.</p>
+            <p>Used ReactJS to create component, use React Router to navigate between pages.</p>
+            <p>Improve performance by using Developer tools, React lazy.</p>
+            <p>Used: <b><i>ReactJs, Hooks, HTML, CSS module </i></b><span>and depploy to </span><b><i>Netlify</i></b></p>
           </div>
           <div className={styles.slideshow}>
             <Webtoon />
@@ -28,12 +28,12 @@ export default function Projects() {
               <h2>Flashcard</h2>
             </a>
             <span><i>Personal project</i></span>
-            <p>Create a website that stores terms and its meaning</p>
-            <p>Used Material UI to create React component</p>
-            <p>Create a simple RestAPI with Python Django, deploy the API to Heroku</p>
-            <p>Used Redux Toolkit to send and retrive data from API, store this data in Redux store as global props</p>
-            <p>Create PWA</p>
-            <p>Used: <b><i>ReactJs, Hooks, HTML, CSS, Python Django, RestAPI </i></b><span>and depploy React app to </span><b><i>Netlify, </i></b><span>depploy RestAPI to </span><b><i>Heroku</i></b></p>
+            <p>Create a website that stores terms and its meaning.</p>
+            <p>Used ReactJS, MaterialUI to create component, use React Router to navigate between pages.</p>
+            <p>Create a simple RestAPI with Python Django REST, deploy the API to Heroku.</p>
+            <p>Used Redux Toolkit to send and retrieve data from API, store this data in Redux store as global props.</p>
+            <p>Make this app a PWAs.</p>
+            <p>Used: <b><i>ReactJs, Hooks, HTML, CSS, Python Django REST </i></b><span>and depploy React app to </span><b><i>Netlify, </i></b><span>depploy RestAPI to </span><b><i>Heroku.</i></b></p>
           </div>
           <div className={styles.slideshow}>
             <Flashcard />
